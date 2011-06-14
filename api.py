f7f944ce83f73a3f2912df4c4da9890a512e1283
Quentin Stafford-Fraser <quentin@pobox.com>
2011-06-14 17:44:16 +0100

    Lots more documentation

--- api.py
+++ api.py
@@ -1,35 +1,93 @@
 #! /usr/bin/python
 #
-# A front end to the CODA API.
-# For more information, see http://support.camvine.com/kb/api/
+# A simple front end to the CODA API.
+# For more information about the API, see http://support.camvine.com/kb/api/
 #
-# Copyright 2010 Cambridge Visual Networks Ltd - Quentin Stafford-Fraser
-
-# Example use:
-#    CONSUMER_KEY = '50d83d409d7745d0'
-#    CONSUMER_SECRET = '564a288948ba25b0'
+# Copyright 2011 Cambridge Visual Networks Ltd - Quentin Stafford-Fraser
+#
+# How to use
+# ==========
+#
+# The most complex bits happen at the beginning and are to do with authentication.
+# This is a pity, but once you've done this, the rest is easy.
+#
+# You'll need a Consumer Key and Secret for your particular application.
+# You can create these by going to:
+#
+#   https://www.codaview.com/gui/developer/
+#
+# and hard-code them into your app.
+#
+# Your app can then identify itself to the Coda server:
+#
+#    CONSUMER_KEY = 'XXXXXXXXXXXXXX'
+#    CONSUMER_SECRET = 'YYYYYYYYYYYYYY'
 #    from pycoda.api import CodaServer
 #    s = CodaServer(CONSUMER_KEY, CONSUMER_SECRET)
 #
-# If you don't have an access token, do this:
+# Now, when a particular user runs your application, it will need an 'access token', 
+# which represents the app's authority to connect to CODA and pretend to be that user.
+#
+# If you don't have an access token for the user, you need to do this:
+#
 #    (rtok, url) = s.get_auth()
-# When user has visited url and approved the request, you can get the access token:
+#
+# The user should go to the resulting and grant access to their account,  
+# just as they would with a Flickr, Facebook or Twitter app. 
+#
+# Once the user has visited url and approved the request, you can get the
+# access token:
+#
 #    atok = s.get_access_token(rtok)
-# which can be stored somewhere.
+#
+# which can be stored somewhere - in a file, in a database, whatever is appropriate
+# for the particular app.
 #
 # An access token atok will look something like: 
 #    "oauth_token_secret=8rGSdemBs2zne2yV&oauth_token=8a8u79TUnbWKs3Bp" 
 #
-# Then get a Coda object from the server:
+# OK - that's the complicated stuff.  The rest is easy.
+#
+# Get a Coda object from the server:
+#
 #    c = s.get_coda(atok)
+#
+# Ask it for a list of displays in my organisation:
+#
 #    print c.getDisplays()
 #
-# Note that, unlike normal Python calls, if you're passing arguments you must use keyword arguments 
-# because the keywords get turned into parameter names.   In other words:
+# Get a list of content sources that have the word 'NASA' in their name:
+# 
+#    srcs = c.getSources(name='NASA')
+#
+# And print the first one.
+#
+#    print srcs[0]
+#
+# Almost all objects in CODA have a unique identifier or 'UUID', which looks something
+# like this: 3c554dfe-f094-5f7e-0010-000000006c43
+# That's what you use to refer to the item, be it a source or display, in other calls.  
+# Let's get the UUID of this source:
+#
+#    print srcs[0]['source_uuid']
+#
+# If you want to put a particular source on a particular display, you say:
+#
+#    c.assignSource(source_uuid=su, display_uuids=[ du ])
+#
+# where su and du are the source_uuid and display_uuid required.
+# Note that the display_uuids parameter is a list, because you can assign
+# the same source to multiple displays simultaneously.
+#
+# Note that, unlike normal Python calls, if you're passing arguments to calls
+# you *must* use keyword arguments because the keywords get turned automatically 
+# into the parameter names.   In other words:
 #    c.removeUser(user_uuid='xxxxxxxxx')
 # will work but
 #    c.removeUser('xxxxxxxxx')
 # will not, even though the call only takes one parameter.
+#
+# That should give you enough to get started!
 
 CODA_SERVER_URL = 'https://api.codaview.com' # Note no trailing slashe
 API_RELATIVE_URL = "external/v2/json/"  # Note trailing slash
